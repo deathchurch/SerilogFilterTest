7b9e6a8ed8ab68a5bc4442050eea1f7e70c81928
Steve Higgin <steven.higgin@bnpparibas-pf.co.uk>
2020-09-30 10:42:15 +0100

    Add project files.

---
+++ SerilogFilterTest/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
+
+namespace SerilogFilterTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(configuration)
+            //.Filter.ByExcluding("RequestPath in ['/ping', '/live', '/ready']")
+            .WriteTo.Seq("http://localhost:5341")
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
